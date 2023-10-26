800c965299bc61c5edfa7661be0dde6d6a20a8c0
dhe20 <daniel-everett@outlook.com>
2023-10-26 18:13:53 +0100

    Merged w/ Manuel

--- EventGenerator.py
+++ EventGenerator.py
@@ -3,40 +3,63 @@ import random
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import interpolate
-import scipy
+import scipy as sp
+
 
 class EventGenerator(Universe):
-    def __init__(self, galaxy_count = 1, dimension = 3, luminosity_gen_type = "Fixed",
-                 coord_gen_type = "Random", spacing = 1, cluster_coeff = None,
-                 event_count = 1, event_distribution = "Random",
-                 noise_distribution = "gauss"):
-        super().__init__(galaxy_count, dimension, luminosity_gen_type,
-                 coord_gen_type, spacing, cluster_coeff)
+    def __init__(self, dimension = 2, luminosity_gen_type = "Fixed",
+                 coord_gen_type = "Clustered",
+                 cluster_coeff = 2, total_luminosity = 1000, size = 1,
+                 alpha = .3, characteristic_luminosity = 1, min_lum = 0,
+                 max_lum = 1, event_count = 1, event_distribution = "Random",
+                 noise_distribution = "Gauss", contour_type = "BVM",
+                 noise_std = 3, resolution = 400, BVM_c = 3,
+                BVM_k = 25, BVM_kappa = 70):
+
+        super().__init__(dimension = dimension, luminosity_gen_type = luminosity_gen_type,
+                         coord_gen_type = coord_gen_type,
+                         cluster_coeff = cluster_coeff, total_luminosity = total_luminosity,
+                         size = size, alpha = alpha, characteristic_luminosity = characteristic_luminosity,
+                         min_lum = min_lum, max_lum = max_lum
+                         )
+
+
         self.event_distribution = event_distribution
         self.event_count = event_count
-        self.resolution = 100
+        self.resolution = resolution
         self.confidence_levels = [0.9973,  0.9545, 0.6827]
 
+        self.event_choice = event_distribution
 
         self.noise_distribution = noise_distribution
-        self.noise_sigma = self.spacing/50
-        self.BH_galaxies = np.empty((self.event_count), dtype = object)
+        self.noise_sigma = noise_std
+        # self.BH_galaxies = np.empty((self.event_count), dtype = object)
         self.BH_coords = np.zeros((self.event_count, self.dimension))
         self.BH_luminosities = np.zeros((self.event_count))
         self.BH_detected_coords = np.empty((self.event_count, self.dimension))
+
+        self.contour_type = contour_type
+
+        self.BVM_k = BVM_k
+        self.BVM_c = BVM_c
+        self.BVM_kappa = BVM_kappa
+
+
+
         if self.dimension == 2:
-            self.BH_contour_meshgrid = np.meshgrid(np.linspace(-self.spacing/2, self.spacing/2, self.resolution),
-                                                   np.linspace(-self.spacing / 2, self.spacing / 2, self.resolution))
+            self.BH_contour_meshgrid = np.meshgrid(np.linspace(-self.size, self.size, self.resolution),
+                                                   np.linspace(-self.size, self.size, self.resolution))
         if self.dimension == 3:
-            self.BH_contour_meshgrid = np.meshgrid(np.linspace(-self.spacing/2, self.spacing/2, self.resolution),
-                                                   np.linspace(-self.spacing / 2, self.spacing / 2, self.resolution),
-                                                   np.linspace(-self.spacing / 2, self.spacing / 2, self.resolution))
+            self.BH_contour_meshgrid = np.meshgrid(np.linspace(-self.size, self.size, self.resolution),
+                                                   np.linspace(-self.size, self.size, self.resolution),
+                                                   np.linspace(-self.size, self.size, self.resolution))
         self.BH_detected_meshgrid = np.empty((self.event_count, *np.shape(self.BH_contour_meshgrid[0])))
 
 
 
-        self.event_generator = dict({"Random": self.random_galaxy})
-        self.coord_noise_generator = dict({"gauss": self.gauss_noise})
+        self.event_generator = dict({"Random": self.random_galaxy, "Proportional":self.proportional_galaxy})
+        self.coord_noise_generator = dict({"Gauss": self.gauss_noise})
+        self.contour_generator = dict({"Gauss": self.gaussian_2d, "BVM": self.BVMShell})
         self.generate_events(event_distribution = self.event_distribution,
                              noise_distribution = self.noise_distribution)
 
@@ -45,24 +68,32 @@ class EventGenerator(Universe):
         while event_count < self.event_count:
             selected = self.event_generator[event_distribution]()
             noise = self.coord_noise_generator[noise_distribution]()
-            if np.sqrt(np.sum(np.square(self.coords[selected] + noise))) > self.cutoff:
+            if np.sqrt(np.sum(np.square(self.coords[selected] + noise))) > self.max_D:
                 continue
-            self.BH_galaxies[event_count] = self.galaxies[selected]
+            # self.BH_galaxies[event_count] = self.galaxies[selected]
             self.BH_coords[event_count] = self.coords[selected]
             self.BH_detected_coords[event_count] = self.coords[selected] + noise
             self.BH_luminosities[event_count] = self.luminosities[selected]
             if self.dimension == 2:
-                grid = self.gaussian_2d(*self.BH_contour_meshgrid,
-                                                         self.BH_detected_coords[event_count], self.noise_sigma)
-                self.BH_detected_meshgrid[event_count] = grid/np.max(grid)
+                grid = self.contour_generator[self.contour_type](*self.BH_contour_meshgrid,
+                                                        self.BH_detected_coords[event_count],
+                                                        self.noise_sigma)
+                self.BH_detected_meshgrid[event_count] = grid/grid.sum()
             event_count+=1
 
     def random_galaxy(self):
-        return random.randint(0, self.galaxy_count-1)
+        return random.randint(0, self.n-1)
+
+    def proportional_galaxy(self):
+        n_list = list(np.arange(0,self.n))
+        source = random.choices(n_list, weights=self.gal_lum)[0]
+        return source
+
 
     def gauss_noise(self):
-        covar = np.identity(self.dimension)*self.noise_sigma**2
-        return np.random.multivariate_normal([0]*self.dimension, covar)
+        rng = np.random.default_rng()
+        noise = rng.normal(loc=0.0, scale=self.noise_sigma, size=self.dimension)
+        return noise
 
     def plot_universe_and_events(self, show = True):
         fig, ax = self.plot_universe(show = False)
@@ -86,17 +117,56 @@ class EventGenerator(Universe):
             plt.show()
 
     def gaussian_2d(self, x, y, mu, sigma):
-        sig_x = sigma**2
-        sig_y = sigma**2
+        sig_x = self.noise_sigma**2
+        sig_y = self.noise_sigma**2
         sig_xy = 0
-        rv = scipy.stats.multivariate_normal(mu, [[sig_x, sig_xy], [sig_xy, sig_y]])
+        rv = sp.stats.multivariate_normal(mu, [[sig_x, sig_xy], [sig_xy, sig_y]])
         Z = rv.pdf(np.dstack((x, y)))
+        return Z
+
+    def d2_gauss(self, X, Y, u_x, u_y, s_x, s_y):
+        Z = np.exp(-(((X-u_x)/s_x)**2 + ((Y-u_y)/s_y)**2)/2)/(2*np.pi*s_x*s_y)
+        return Z
+
+    def von_misses(self, x, u, kappa):
+        return np.exp(kappa*np.cos(x-u))/(2*np.pi*sp.special.iv(0,kappa))
+
+    def burr(self, x, c, k, l):
+        return (c*k/l)*((x/l)**(c-1))*((1+(x/l)**c)**(-k-1))
+
+
+    def BVMShell(self, x, y, mu, sigma):
+        u_x = mu[0]
+        u_y = mu[1]
+        s_x = self.noise_sigma
+        s_y = self.noise_sigma
+        X = self.BH_contour_meshgrid[0]
+        Y = self.BH_contour_meshgrid[1]
+        r = np.sqrt((X) ** 2 + (Y) ** 2)
+        phi = np.arctan2(Y, X)
+        u_r = np.sqrt((u_x) ** 2 + (u_y) ** 2)
+        u_phi = np.arctan2(u_y, u_x)
+
+        k = self.BVM_k
+        c = self.BVM_c
+        kappa = self.BVM_kappa
+
+        angular = self.von_misses(phi, u_phi, kappa)
+        radial = self.burr(r, c, k, u_r)
+        Z = r * angular * radial
+
+        vals = [self.d2_gauss(u_x + 3 * s_x, u_y + 3 * s_y, u_x, u_y, s_x, s_y),
+                self.d2_gauss(u_x + 2 * s_x, u_y + 2 * s_y, u_x, u_y, s_x, s_y),
+                self.d2_gauss(u_x + s_x, u_y + s_y, u_x, u_y, s_x, s_y)]
         return Z
 
 
 
-Gen = EventGenerator(event_count=2, spacing = 1000, dimension = 2, galaxy_count=10000,
-                     luminosity_gen_type = "Random", coord_gen_type = "Clustered", cluster_coeff = 200)
+Gen = EventGenerator(dimension = 2, size = 50, event_count=3,
+                     luminosity_gen_type = "Shoddy-Schecter", coord_gen_type = "Clustered",
+                     cluster_coeff=5, characteristic_luminosity=.1, total_luminosity=1000,
+                     event_distribution="Proportional")
+
 
 Gen.plot_universe_and_events()
 
--- Universe.py
+++ Universe.py
@@ -7,47 +7,99 @@ import powerbox as pbox
 
 plt.style.use('dark_background')
 class Universe:
-    def __init__(self, galaxy_count = 1, dimension = 3, luminosity_gen_type = "Fixed", coord_gen_type = "Clustered", spacing = 1,
-                 cluster_coeff = None):
+    def __init__(self, dimension = 3, luminosity_gen_type = "Fixed", coord_gen_type = "Clustered",
+                 cluster_coeff = 2, total_luminosity = 1000, size = 1,
+                 alpha = .3, characteristic_luminosity = 1, min_lum = 0,
+                 max_lum = 1):
 
-        self.galaxy_count = galaxy_count
+        self.n = 0
+
+        self.L_0 = total_luminosity
         self.dimension = dimension
-        self.galaxies = np.empty((self.galaxy_count), dtype = object)
-        self.luminosities = np.zeros((self.galaxy_count))
-        self.coords = np.empty((self.galaxy_count,self.dimension))
-        self.spacing = spacing
-        self.cutoff = self.spacing*0.4
+        self.luminosity_func = luminosity_gen_type
+
+        self.alpha = alpha
+        self.size = size
+        self.L_star = characteristic_luminosity
+        self.min_L = min_lum
+        self.max_L = max_lum
+
         self.cluster_coeff = cluster_coeff
+        self.max_D = self.size*0.7
 
-        self.luminosity_generator = dict({"Random": self.random_luminosity, "Fixed": self.fixed_luminosity})
-        self.coord_generator = dict({"Random": self.random_coords, "Clustered": self.clustered_coords})
+        self.luminosity_generator = dict({"Uniform": self.uniform_galaxies,
+                                          "Fixed": self.fixed_luminosity,
+                                          "Cut-Schecter": self.cut_schechter,
+                                          "Shoddy-Schecter":self.schecter_luminosity,
+                                          })
 
-        self.create_galaxies(luminosity_gen_type = luminosity_gen_type, coord_gen_type = coord_gen_type)
+        self.luminosities = self.luminosity_generator[luminosity_gen_type]()
 
-    def create_galaxies(self, luminosity_gen_type, coord_gen_type):
-        self.coords = self.coord_generator[coord_gen_type](self.cluster_coeff)
-        for i in range(self.galaxy_count):
-            luminosity = self.luminosity_generator[luminosity_gen_type]()
-            self.luminosities[i] = luminosity
-            self.galaxies[i] = Galaxy(coords = self.coords[i], dimension = self.dimension, luminosity = luminosity)
+        # self.galaxies = np.empty((self.n), dtype = object)
+
+        self.coord_generator = dict({"Random": self.random_coords,
+                                     "Clustered": self.clustered_coords})
+
+        self.coords = self.coord_generator[coord_gen_type]()
+
+        # self.galaxies[i] = Galaxy(coords=self.coords[i], dimension=self.dimension, luminosity=luminosity)
+
+
+        # self.x = np.linspace(-self.size, self.size, 50*self.size)
+        # self.y = np.linspace(-self.size, self.size, 50*self.size)
+        # self.X, self.Y = np.meshgrid(self.x, self.y)
+
+
+
+
+
+
+        # self.create_galaxies(luminosity_gen_type = luminosity_gen_type, coord_gen_type = coord_gen_type)
+
+
+
 
     def fixed_luminosity(self):
-        return 1
+        self.n = round(self.L_0/self.L_star)
+        return [self.L_star]*self.n
 
-    def random_luminosity(self):
-        return random.random()
+    def schecter_luminosity(self):
+        N_0 = self.L_0 / (self.L_star * (1 + self.alpha))
+        self.n = round(N_0)
+        rng = np.random.default_rng()
+        self.gal_lum = rng.gamma(self.alpha, scale=self.L_star, size=self.n)
+        # if any(self.gal_lum == 0.0):
+        #     s = set(self.gal_lum)
+        #     self.gal_lum += sorted(s)[1]
+        return self.gal_lum
 
-    def random_coords(self, cluster_coeff):
-        random_coords = np.zeros((self.galaxy_count, self.dimension))
-        for i in range(self.galaxy_count):
-            random_coords[i] = np.array([(random.random()-0.5)*self.spacing for _ in range(self.dimension)])
+    def cut_schechter(self):
+        N_0 = self.L_0 / (self.L_star * (1 + self.alpha))
+        self.n = round(N_0)
+        rng = np.random.default_rng()
+        gal_lum = []
+        while len(gal_lum) < self.n:
+            l = rng.gamma(self.alpha, scale=self.L_star, size=1)[0]
+            if l > self.min_L:
+                gal_lum.append(l)
+        self.gal_lum = np.array(gal_lum)
+        return self.gal_lum
+
+    def uniform_galaxies(self):
+        N_0 = self.L_0 / (0.5 * (self.min_L + self.max_L))
+        self.n = round(N_0)
+        self.gal_lum = np.random.uniform(0, 1, size=self.n)
+        return self.gal_lum
+
+    def random_coords(self):
+        random_coords = np.zeros((self.n, self.dimension))
+        for i in range(self.n):
+            random_coords[i] = np.array([(random.random()-0.5)*2*self.size for _ in range(self.dimension)])
         return random_coords
 
-    def clustered_coords(self, cluster_coeff):
-        if cluster_coeff == None:
-            cluster_coeff = 2
+    def clustered_coords(self):
 
-        power = lambda k: cluster_coeff * k ** -3
+        power = lambda k: self.cluster_coeff * k ** -3
 
 
         lnpb = pbox.LogNormalPowerBox(
@@ -58,14 +110,14 @@ class Universe:
             # seed=self.seed  # Set a seed to ensure the box looks the same every time (optional)
         )
 
-        num_of_galaxies = self.galaxy_count
+        num_of_galaxies = self.n
         clustered_sample = lnpb.create_discrete_sample(nbar=int(2 * num_of_galaxies),
                                                       randomise_in_cell=True,  # nbar specifies the number density
                                                       # min_at_zero=False  # by default the samples are centred at 0. This shifts them to be positive.
                                                       )
         index_of_galaxies = list(np.arange(0, len(clustered_sample), 1))
         selected_index = random.sample(index_of_galaxies, k=num_of_galaxies)
-        selected_galaxies = clustered_sample[selected_index, :]*self.spacing
+        selected_galaxies = clustered_sample[selected_index, :]*self.size*2
 
         return selected_galaxies
 
@@ -73,24 +125,31 @@ class Universe:
         x, y = zip(*self.coords)
         fig, ax = plt.subplots()
 
-        ax.set_ylim(-0.5*self.spacing, 0.5*self.spacing)
-        ax.set_xlim(-0.5*self.spacing, 0.5*self.spacing)
+        ax.set_ylim(-self.size, self.size)
+        ax.set_xlim(-self.size, self.size)
         fig.set_figheight(8)
         fig.set_figwidth(8)
-        cutoff = plt.Circle((0, 0), self.cutoff, color='w', ls="--", fill="")
+        cutoff = plt.Circle((0, 0), self.max_D, color='w', ls="--", fill="")
         ax.add_patch(cutoff)
         for (x, y, s) in zip(x, y, self.luminosities):
-            ax.add_artist(plt.Circle(xy=(x, y), radius=s, color="y"))
-        ax.scatter(0,0, s=self.spacing/2.5, c = "w", marker = "x")
-        ax.xaxis.set_tick_params(labelbottom=False)
-        ax.yaxis.set_tick_params(labelleft=False)
-        ax.set_xticks([])
-        ax.set_yticks([])
+            ax.add_artist(plt.Circle(xy=(x, y), radius=s+0.001*self.L_star, color="y"))
+        ax.scatter(0,0, s=self.size/1.25, c = "w", marker = "x")
+        # ax.xaxis.set_tick_params(labelbottom=False)
+        # ax.yaxis.set_tick_params(labelleft=False)
+        # ax.set_xticks([])
+        # ax.set_yticks([])
         plt.tight_layout()
         if show:
             plt.show()
         return fig, ax
 #
-# Gen = Universe(spacing = 200, dimension = 2, galaxy_count=1000, luminosity_gen_type = "Fixed", coord_gen_type = "Clustered",
-#                cluster_coeff=50)
-# Gen.plot_universe()
+Gen = Universe(size = 50, dimension = 2,
+               luminosity_gen_type = "Cut-Schecter", coord_gen_type = "Random",
+               cluster_coeff=50, characteristic_luminosity=.01, total_luminosity=100)
+Gen.plot_universe()
+
+# dict({"Uniform": self.uniform_galaxies,
+#                   "Fixed": self.fixed_luminosity,
+#                   "Cut-Schecter": self.cut_schechter,
+#                   "Shoddy-Schecter":self.schecter_luminosity,
+#                   })
