underflow fix and plotting

Components/Inference.py
@@ -283,15 +283,31 @@ class Inference(SurveyAndEventData):
         full_expression = burr_full * vmf * luminosity_term
         
         self.H_0_pdf_single_event = np.sum(full_expression, axis=2)
-        self.H_0_pdf = np.product(self.H_0_pdf_single_event, axis=0)
-
+        #self.H_0_pdf = np.product(self.H_0_pdf_single_event, axis=0)
+        '''
         if self.p_det:
             p_det_vec = luminosity_term * self.get_p_det_vec(Ds)
             P_det_total = np.sum(p_det_vec, axis=1)
             self.P_det_total = P_det_total
             P_det_total_power = np.power(P_det_total, self.SurveyAndEventData.detected_event_count)
             self.H_0_pdf = self.H_0_pdf/P_det_total_power
-        
+        '''
+        if self.p_det:
+            p_det_vec = luminosity_term * self.get_p_det_vec(Ds)
+            P_det_total = np.sum(p_det_vec, axis=1)
+            self.P_det_total = P_det_total
+            self.H_0_pdf_single_event = self.H_0_pdf_single_event / P_det_total
+
+        f = np.vectorize(math.frexp)
+        split = f(self.H_0_pdf_single_event)
+        flo = split[0]
+        ex = split[1]
+        p_flo = np.prod(flo, axis=0)
+        p_ex = np.sum(ex, axis=0)
+        scaled_ex = p_ex - np.max(p_ex)
+        scaled_flo = p_ex / p_flo[np.argmax(p_ex)]
+        self.H_0_pdf = scaled_flo * (0.5 ** (-1 * scaled_ex))
+
         self.H_0_pdf /= np.sum(self.H_0_pdf) * (self.H_0_increment)
         return self.H_0_pdf
 

Sampling/Luminosity_functions_sampler.py
@@ -391,9 +391,9 @@ for i in tqdm(range(len(investigated_values))):
 
 # RUN THIS ONE
 
-investigated_characteristic = 'trial_survey_completeness'
+investigated_characteristic = 'trial_survey_completeness_wrong_2'
 #investigated_values = [25,75,95]
-investigated_values = np.array([0.5]) #,0.5])
+investigated_values = np.array([0.05,0.1,0.2,0.3]) #,0.5])
 investigated_values /= (4*np.pi*(0.4*625)**2)
 max_numbers = []
 percentage = []
@@ -401,8 +401,28 @@ percentage = []
 #f = []
 
 for i in tqdm(range(len(investigated_values))):
-    Investigation = Sampler(universe_count = 60, min_flux=investigated_values[i], completeness_type='cut_lim', p_det=True, gamma = False, event_distribution='Proportional', total_luminosity=2000/3, wanted_det_events = 50, specify_event_number = True, 
-                            noise_distribution='BVMF_eff', event_distribution_inf='Proportional', lum_function_inf='Full-Schechter', flux_threshold=1, investigated_characteristic = investigated_characteristic, investigated_value = investigated_values[i])
+    Investigation = Sampler(universe_count = 500, min_flux=investigated_values[i], completeness_type='cut_lim', p_det=True, gamma = False, event_distribution='Random', total_luminosity=2000/3, wanted_det_events = 50, specify_event_number = True, 
+                            noise_distribution='BVMF_eff', event_distribution_inf='Random', luminosity_gen_type='Shoddy-Schechter' , lum_function_inf='Shoody-Schechter', flux_threshold=0, investigated_characteristic = investigated_characteristic, investigated_value = investigated_values[i])
+    Investigation.Sample()
+    percentage.append(Investigation.survey_percentage)
+    #b.append(Investigation.burr_i)
+    #f.append(Investigation.full)
+    max_numbers.append(Investigation.max_num)
+
+#%%
+    
+investigated_characteristic = 'trial_survey_completeness_right_2'
+#investigated_values = [25,75,95]
+investigated_values = np.array([0.05,0.1,0.2,0.3]) #,0.5])
+investigated_values /= (4*np.pi*(0.4*625)**2)
+max_numbers = []
+percentage = []
+#b = []
+#f = []
+
+for i in tqdm(range(len(investigated_values))):
+    Investigation = Sampler(universe_count = 100, min_flux=investigated_values[i], completeness_type='cut_lim', p_det=True, gamma = False, event_distribution='Random', total_luminosity=2000/3, wanted_det_events = 50, specify_event_number = True, 
+                            noise_distribution='BVMF_eff', event_distribution_inf='Random', luminosity_gen_type='Shoddy-Schechter', lum_function_inf='Shoddy-Schechter', flux_threshold=1, investigated_characteristic = investigated_characteristic, investigated_value = investigated_values[i])
     Investigation.Sample()
     percentage.append(Investigation.survey_percentage)
     #b.append(Investigation.burr_i)
@@ -470,7 +490,7 @@ for i in range(len(investigated_values)):
 ax1.tick_params(axis='both', which='major', labelsize=20)
 ax2.tick_params(axis='both', which='major', labelsize=20)
 
-ax1.violinplot(meanss, bw_method=0.1, vert=False, showmeans=True)
+ax1.violinplot(meanss, bw_method=0.4, vert=False, showmeans=True)
 ax1.set_yticks(pos)
 ax1.set_yticklabels(investigated_values, fontsize=20)
 ax1.set_title('Means', fontsize = 25)
@@ -484,6 +504,19 @@ fig.supylabel(axis_namer(investigated_characteristic), fontsize=20)
 ax2.grid(axis='x')
 
 plt.show()
+
+#%%
+
+def expected(data, sig):
+    sig = np.array(sig)
+    S = np.sum(1/sig**2)
+    data = np.array(data)
+    return np.sum(data/(sig**2))/S, np.sqrt(1/S)
+
+for i in range(len(investigated_values)):
+    bias, bias_err = expected(meanss[i], stdss[i])    
+    s = 'H_0 = {:.2f}+/-{:.2f}'.format(bias, bias_err)
+    print(s)
 
 #%%
 
@@ -508,12 +541,6 @@ for i in ax:
 plt.subplots_adjust(wspace=0, hspace=0)
 
 b = 20
-
-def expected(data, sig):
-    sig = np.array(sig)
-    S = np.sum(1/sig**2)
-    data = np.array(data)
-    return np.sum(data/(sig**2))/S, np.sqrt(1/S)
 
 
 #bias_0 = np.mean(meanss[0])-70
@@ -875,7 +902,7 @@ for axis in ['top','bottom','left','right']:
 image_format = 'svg' # e.g .png, .svg, etc.
 image_name = 'Plots//redshift_uncertainty.svg'
 
-plt.savefig(image_name, format=image_format,  bbox_inches='tight', pad_inches=0.5, dpi=1200)
+#plt.savefig(image_name, format=image_format,  bbox_inches='tight', pad_inches=0.5, dpi=1200)
 
 plt.show()
 
@@ -981,9 +1008,124 @@ for axis in ['top','bottom','left','right']:
 image_format = 'svg' # e.g .png, .svg, etc.
 image_name = 'Plots//survey_completeness_many.svg'
 
-plt.savefig(image_name, format=image_format,  bbox_inches='tight', pad_inches=0.5, dpi=1200)
+# plt.savefig(image_name, format=image_format,  bbox_inches='tight', pad_inches=0.5, dpi=1200)
 
 plt.show()
 
+
+# %%
+
+# Survey completeness
+
+
+fig = plt.figure(figsize = (12,10))
+# create grid for different subplots
+spec = gridspec.GridSpec(ncols=1, nrows=4,
+                         wspace=0.2,
+                         hspace=0.)
+
+ax1 = fig.add_subplot(spec[0])
+ax2 = fig.add_subplot(spec[1])
+ax3 = fig.add_subplot(spec[2])
+ax4 = fig.add_subplot(spec[3])
+ax = [ax1,ax2, ax3, ax4]
+
+for i in ax:
+    i.tick_params(axis='both', which='major', direction='in', labelsize=30, size=8, width=3, pad = 9)
+    i.tick_params(axis='both', which='major', direction='in', labelsize=30, size=8, width=3, pad = 9)
+    i.tick_params(axis='both', which='major', direction='in', labelsize=30, size=8, width=3, pad = 9)
+
+ax1.set_ylim(0,0.49)
+ax1.set_xlim(50,90)
+ax2.set_ylim(0,0.4)
+ax2.set_xlim(50,90)
+ax3.set_ylim(0,0.2)
+ax3.set_xlim(50,90)
+ax4.set_ylim(0,0.2)
+ax4.set_xlim(50,90)
+
+plt.subplots_adjust(wspace=0, hspace=0)
+
+b = 16
+bias_0 = np.mean(meanss[0])-70
+bias_1 = np.mean(meanss[1])-70
+bias_2 = np.mean(meanss[2])-70
+bias_3 = np.mean(meanss[3])-70
+bias_err_0 = np.std(meanss[0])
+bias_err_1 = np.std(meanss[1])
+bias_err_2 = np.std(meanss[2])
+bias_err_3 = np.std(meanss[3])
+
+
+bias_0, bias_err_0 = expected(meanss[0], stdss[0])
+bias_1, bias_err_1 = expected(meanss[1], stdss[1])
+bias_2, bias_err_2 = expected(meanss[2], stdss[2])
+bias_3, bias_err_3 = expected(meanss[3], stdss[3])
+
+
+bias_0 -= 70
+bias_1 -= 70
+bias_2 -= 70
+bias_3 -= 70
+
+ax1.hist(meanss[0], bins=b, histtype='step', edgecolor='turquoise', lw=3, density=1)
+ax2.hist(meanss[1], bins=b, histtype='step', edgecolor='turquoise', lw=3, density=1)
+ax3.hist(meanss[2], bins=b, histtype='step', edgecolor='turquoise', lw=3, density=1)
+ax4.hist(meanss[3], bins=b, histtype='step', edgecolor='turquoise', lw=3, density=1)
+
+ax1.vlines(x=bias_0+70, ymin=0, ymax=0.49,ls='dashed', lw=3, label='$\hat H_0-H_0={:.2f}\pm{:.2f}$'.format(bias_0, bias_err_0))
+ax2.vlines(x=bias_1+70, ymin=0, ymax=0.49,ls='dashed', lw=3, label='$\hat H_0-H_0={:.2f}\pm{:.2f}$'.format(bias_1, bias_err_1))
+ax3.vlines(x=bias_2+70, ymin=0, ymax=0.49,ls='dashed', lw=3, label='$\hat H_0-H_0={:.2f}\pm{:.2f}$'.format(bias_2, bias_err_2))
+ax4.vlines(x=bias_3+70, ymin=0, ymax=0.49,ls='dashed', lw=3, label='$\hat H_0-H_0={:.2f}\pm{:.2f}$'.format(bias_3, bias_err_3))
+
+ax1.vlines(x=70, ymin=0, ymax=0.49, color='r', ls='dashed', lw=3)
+ax2.vlines(x=70, ymin=0, ymax=0.49, color='r', ls='dashed', lw=3)
+ax3.vlines(x=70, ymin=0, ymax=0.49, color='r', ls='dashed', lw=3)
+ax4.vlines(x=70, ymin=0, ymax=0.49, color='r', ls='dashed', lw=3)
+
+
+#ax1.grid(axis='x', ls='dashed', alpha=0.5)
+#ax2.grid(axis='x', ls='dashed', alpha=0.5)
+ax1.legend(fontsize=20)
+ax2.legend(fontsize=20)
+ax3.legend(fontsize=20)
+ax4.legend(fontsize=20)
+ax1.set_xticklabels([])
+ax2.set_xticklabels([])
+ax2.set_yticks([0.0,0.15,0.30])
+ax3.set_yticks([0.0,0.08,0.16])
+ax4.set_yticks([0.0,0.08,0.16])
+
+per = []
+for i in range(len(percentage)):
+    per.append(100*(np.mean(percentage[i])))
+
+
+ax1.annotate(r'$F_{{\mathrm{{min}}}}/F_* = {}$'.format(l_lim[0]), xy=(53,0.3),xytext=(52.5,0.3), fontsize=35)
+ax2.annotate(r'$F_{{\mathrm{{min}}}}/F_* = {}$'.format(l_lim[1]), xy=(53,0.4*0.3/0.49),xytext=(52.5,0.4*0.3/0.49), fontsize=35)
+ax3.annotate(r'$F_{{\mathrm{{min}}}}/F_* = {}$'.format(l_lim[2]), xy=(53,0.08 *0.3/0.49),xytext=(52.5,0.2*0.3/0.49), fontsize=35)
+ax4.annotate(r'$F_{{\mathrm{{min}}}}/F_* = {}$'.format(l_lim[3]), xy=(53,0.08 *0.3/0.49),xytext=(52.5,0.2*0.3/0.49), fontsize=35)
+
+ax1.annotate(r'$f_{{\mathrm{{det}}}} = {:.0f}\%$'.format(per[0]), xy=(53,0.3),xytext=(52.5,0.17), fontsize=35)
+ax2.annotate(r'$f_{{\mathrm{{det}}}} = {:.0f}\%$'.format(per[1]), xy=(53,0.4*0.3/0.49),xytext=(52.5,0.4*0.17/0.49), fontsize=35)
+ax3.annotate(r'$f_{{\mathrm{{det}}}} = {:.0f}\%$'.format(per[2]), xy=(53,0.08 *0.3/0.49),xytext=(52.5,0.2*0.17/0.49), fontsize=35)
+ax4.annotate(r'$f_{{\mathrm{{det}}}} = {:.0f}\%$'.format(per[3]), xy=(53,0.08 *0.3/0.49),xytext=(52.5,0.2*0.17/0.49), fontsize=35)
+
+ax4.set_xlabel(r'$\hat H_0$ (km s$^{-1}$ Mpc$^{-1}$)',fontsize=35, labelpad=15)
+
+ax1.set_title('Survey completenes Wrong', fontsize=40, pad=30)
+
+for axis in ['top','bottom','left','right']:
+    ax1.spines[axis].set_linewidth(3)
+    ax2.spines[axis].set_linewidth(3)
+    ax3.spines[axis].set_linewidth(3)
+    ax4.spines[axis].set_linewidth(3)
+
+image_format = 'svg' # e.g .png, .svg, etc.
+image_name = 'Plots//survey_completeness_many.svg'
+
+# plt.savefig(image_name, format=image_format,  bbox_inches='tight', pad_inches=0.5, dpi=1200)
+
+plt.show()
 
 # %%