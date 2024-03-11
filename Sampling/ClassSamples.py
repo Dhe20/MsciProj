be59be5a29dafd68808d4a608694d4b489f6fb5f
manuellcoelho <mancoelho02@gmail.com>
2024-03-11 12:24:07 +0000

    m

--- Sampling/ClassSamples.py
+++ Sampling/ClassSamples.py
@@ -26,7 +26,7 @@ class Sampler():
                 wanted_det_events = 50, specify_event_number = False, luminosity_gen_type="Full-Schechter", coord_gen_type="Random",
                 cluster_coeff=0, characteristic_luminosity=1, lower_lim=0.1, total_luminosity=3333.333,
                 BVM_c = 15, BVM_k = 2, BVM_kappa = 200, event_distribution="Proportional", noise_distribution="BVMF_eff", redshift_noise_sigma=0, noise_sigma=5,
-                resolution=10, plot_contours=False, alpha = 0.3, beta=-1.5, min_flux=0, survey_incompleteness=0, completeness_type='cut_lim', resolution_H_0 = 200, 
+                resolution=10, plot_contours=False, alpha = 0.3, beta=-1.5, min_flux=0, survey_incompleteness=0, completeness_type='cut_lim', DD = 0, resolution_H_0 = 200, 
                 H_0_Min = 50, H_0_Max = 100, universe_count = 200, survey_type = "perfect", gamma=True, gauss=False, p_det=False, event_distribution_inf='Proportioanl', lum_function_inf = 'Full-Schechter', poster=False, flux_threshold=0,
                 investigated_characteristic='0', investigated_value=0, save_normally = True):
 
